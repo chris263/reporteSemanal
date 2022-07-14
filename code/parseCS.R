8c80f45f8858bb787d8ae8919ef200f7ad3e56bd
chris simoes <ccs263@cornell.edu>
2022-07-14 15:50:53 -0300

    adding 4141vip3 synonym

--- code/parseCS.R
+++ code/parseCS.R
@@ -125,45 +125,6 @@ read_fun <- function(inLista){
 
 }
 
-#filtrando para locais que possuem dados de corn stunt
-filter_locais <- function(inLoc, stg, yrs, crt5, minEnter, maxEnter,inSea){
-  # inLoc <- myDF
-  # stg <- Stage
-  # yrs <- as.numeric(years)
-  # crt5 <- filterYear
-  # minEnter <- 50
-  # maxEnter <- 200
-  # inSea <- "Winter"
-
-  inLoc <- inLoc %>% filter(Season==inSea) %>% filter(Year==selYear) %>% filter(Stage == stg)
-
-  unique(inLoc$local)
-
-  entLoc <- as.data.frame(tapply(inLoc$genotipo, inLoc$local,length))
-  entLoc <- entLoc %>% mutate(local = rownames(entLoc))
-  colnames(entLoc)[1] <- "Entradas"
-  entLoc <- entLoc %>% filter(Entradas > minEnter) %>% filter(Entradas < maxEnter) %>% arrange(Entradas)
-
-  vLoc <- entLoc$local
-
-  y1 <- max(yrs)
-  y2 <- yrs[2]
-  y3 <- min(yrs)
-
-  if(crt5 == "yes"){
-    stg2 <- stg-1
-    inLoc$Stage <- as.numeric(inLoc$Stage)
-    outLoc1 <- inLoc[which(inLoc$local%in%vLoc),] %>% filter(Year== y1)
-    outLoc2 <- inLoc[which(inLoc$local%in%vLoc),] %>% filter(Year== y2)
-    outLoc3 <- inLoc[which(inLoc$local%in%vLoc),] %>% filter(Year== y3)
-
-    outLoc <- rbind(outLoc1, outLoc2, outLoc3)
-  }else{
-    outLoc <- inLoc %>% filter(local %in% vLoc)
-  }
-
-  return(outLoc)
-}
 
 sinoFun <- function(myData, sinIN){
   # sinIN <- sinN
@@ -201,39 +162,53 @@ sinoFun <- function(myData, sinIN){
       next
     }
     getPosition <- grep(myOld,myData$genotipo)
-
     myData$genotipo <- replace(myData$genotipo, getPosition, myNew)
-
   }
   myData$genotipo <- gsub("\\_","\\\\", myData$genotipo)
   return(myData)
 }
 
 
-parsing_data <- function(stageData, nomes_entrada){
-  # Criando um vetor para guardar o numero da coluna
-  index_nomes <- c(1:length(nomes_selecionados))
 
-  for (i in 1:length(nomes_selecionados)){
-     index_nomes[i] <- which( colnames(stageData)==nomes_entrada[i] )
-     colnames(stageData)[index_nomes[i]] <- novos_nomes[i]
-   }
+count_fun_teste <- function(inData, inYR, inSE, inGE){
 
-  stageData$genotipo <- gsub(" ", "", stageData$genotipo, fixed = TRUE)
-  stageData$local <- gsub(" ", "", stageData$local, fixed = TRUE)
+  # inData = countDF
+  # inYR = c(21,22)
+  # inSE = c("WN")
+  # inGE = c("SZD5031VIP3")
 
-  stageData$genotipo <- toupper(stageData$genotipo)
-  stageData$local <- toupper(stageData$local)
 
-  stageOut <- stageData %>% select(year, local, plotName, Rep, genotipo, Nota, yield)
-  filter_locais(stageOut)
+  countDF <- aggregate(Nota ~ genotipo + BU + trait + year + season, data = inData, FUN = length)
+  dataPointsTable<- count_fun(tableTraits,allData,"local") # arquivo parseCS.R
+
+
+  if(inGE == "all"){
+    inData <- inData %>% filter(year %in% inYR, season %in% inSE)
+  }else{
+    inData <- inData %>% filter(year %in% inYR, season %in% inSE, genotipo %in% inGE)
+  }
+
+  inDF1<- data.frame(tapply(inData$BU, list(inData$BU,inData$trait),length))
+
+  inDF2<- data.frame(tapply(inData$Nota, inData$trait, length))
+  inDF2$trait <- rownames(inDF2)
+
+  inDF3 <- data.frame(colSums(!is.na(inDF1)))
+  inDF3$trait <- rownames(inDF3)
+
+  outDF <- left_join(inDF2, inDF3, by=c("trait"="trait"))
+  colnames(outDF) <- c("dataPoints","trait","Locais")
+  outDF <-outDF %>% relocate(trait, .before = dataPoints)
+
+  return(outDF)
 }
 
 
-count_fun <- function(inTable,inData, inType){
+count_fun <- function(inTable, inData, inType){
   # inTable = tableTraits
-  # inData = myCountData
+  # inData = allData_filter
   # inType <- "genotipos"
+  # Funcao antiga para contar
 
   if(inType == "local"){
     for(i in 1:length(inTable$codigo)){
